1a1084d0a5c424207f3de5143d12672edb9a8292
Suhail Muhammad <suhailmuhammed8547@gmail.com>
2025-04-09 16:39:14 +0530

    api changes

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // your backend base URL
+  baseURL: 'https://restaurant-menu-backend-2qio.onrender.com/api', // your backend base URL
 });
 
 export default api;
